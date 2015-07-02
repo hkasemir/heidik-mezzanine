63ae9e174cb5423f4aad54459aeae865f4e7e6ec
hkasemir <heidi.kasemir@gmail.com>
2015-07-02 16:47:03 -0400

    updates to homepage

--- urls.py
+++ urls.py
@@ -100,7 +100,4 @@ handler500 = "mezzanine.core.views.server_error"
 urlpatterns += patterns('',
     url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
         'document_root': settings.MEDIA_ROOT,
-    }),
-    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {
-        'document_root': settings.STATIC_ROOT,
     }),)
