5a62313595a2d7cb6a691da76c9bd1175ca699ad
hkasemir <heidi.kasemir@gmail.com>
2015-07-02 12:36:12 -0400

    removed the if statement to check debug status. This will allow me to set debug to false (hopefully) while also using django to serve the static files, because apache seems to have an issue with the .thumbnails directories

--- urls.py
+++ urls.py
@@ -96,12 +96,11 @@ urlpatterns += patterns('',
 handler404 = "mezzanine.core.views.page_not_found"
 handler500 = "mezzanine.core.views.server_error"
 
-if settings.DEBUG:
-    urlpatterns += patterns('',
-        url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
-            'document_root': settings.MEDIA_ROOT,
-        }),
-        url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {
-            'document_root': settings.STATIC_ROOT,
-        }),
-)
+
+urlpatterns += patterns('',
+    url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+        'document_root': settings.MEDIA_ROOT,
+    }),
+    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {
+        'document_root': settings.STATIC_ROOT,
+    }),)
