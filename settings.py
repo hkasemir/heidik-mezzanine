2f0d119e331b6be7ea088f1d76c114546a6c66b9
Heidi Kasemir <heidi.kasemir@gmail.com>
2015-10-03 21:55:32 -0700

    added beautifulsoup4 for html pretty formatting

--- settings.py
+++ settings.py
@@ -275,6 +275,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
 # response phase the middleware will be applied in reverse order.
 MIDDLEWARE_CLASSES = (
     "mezzanine.core.middleware.UpdateCacheMiddleware",
+    "heidik.BeautifulMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.locale.LocaleMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
