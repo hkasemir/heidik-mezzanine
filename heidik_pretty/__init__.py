f5294b483149a8a9619327807a5a7e5e00045a2e
Heidi Kasemir <heidi.kasemir@gmail.com>
2015-10-05 11:56:47 -0700

    fixed bug in tree menus where active class was not being added, and bs4 in admin panel was glitching editing of pages

--- heidik_pretty/__init__.py
+++ heidik_pretty/__init__.py
@@ -2,8 +2,11 @@ from bs4 import BeautifulSoup
 
 class BeautifulMiddleware(object):
     def process_response(self, request, response):
+        if request.path.startswith('/admin'):
+            return response
+
         if response.status_code == 200:
             if response["content-type"].startswith("text/html"):
-                beauty = BeautifulSoup(response.content, 'html5')
+                beauty = BeautifulSoup(response.content, 'html5lib')
                 response.content = beauty.prettify()
         return response
